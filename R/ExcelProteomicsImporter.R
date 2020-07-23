05cb8e6c2b5c0d9768ec46e72ec21db45879fbf0
paul-shannon <paul.thurmond.shannon@gmail.com>
2020-07-22 19:02:43 -0700

    small start

--- R/ExcelProteomicsImporter.R
+++ R/ExcelProteomicsImporter.R
@@ -17,6 +17,10 @@
                           )
 #------------------------------------------------------------------------------------------------------------------------
 setGeneric('getRawTable', signature='obj', function(obj) standardGeneric ('getRawTable'))
+setGeneric('validateColumnNames', signature='obj', function(obj) standardGeneric ('validateColumnNames'))
+setGeneric('getGroupCount', signature='obj', function(obj) standardGeneric ('getGroupCount'))
+setGeneric('identifyGroupBoundaries', signature='obj', function(obj) standardGeneric ('identifyGroupBoundaries'))
+setGeneric('getGroupBoundaries', signature='obj', function(obj) standardGeneric ('getGroupBoundaries'))
 #------------------------------------------------------------------------------------------------------------------------
 #' Create a ExcelProteomicsImporter connection
 #'
@@ -48,6 +52,98 @@ setMethod('getRawTable', 'ExcelProteomicsImporter',
 
      function(obj){
         obj@rawTable
+        })
+
+#------------------------------------------------------------------------------------------------------------------------
+#' learn experiment conditions and timepoints from the column names
+#'
+#' @rdname validateColumnNames
+#' @aliases validateColumnNames
+#'
+#' @export
+#'
+setMethod('validateColumnNames', 'ExcelProteomicsImporter',
+
+     function(obj){
+        tbl <- obj@rawTable
+        colnames <- colnames(tbl)
+        stopifnot("PeptideLabel" %in% colnames | "Peptide.Label" %in% colnames)
+        if("PeptideLabel" %in% colnames)
+            analytes <- tbl$PeptideLabel
+        if("Peptide.Label" %in% colnames)
+            analytes <- tbl[, "Peptide.Label"]
+
+        stopifnot(length(unique(analytes)) == nrow(tbl))
+
+        stopifnot(any(grepl("avg", colnames)))
+        stopifnot(any(grepl("stdev", colnames)))
+        avg.cols <- grep("avg", colnames)
+        sd.cols  <- grep("stdev", colnames)
+        stopifnot(length(avg.cols) == length(sd.cols))
+
+        groupCount <- length(avg.cols)   # expect 1 or 2
+        stopifnot(groupCount %in% c(1,2))
+        obj@state$groupCount <- groupCount
+        return(TRUE)
+        })
+
+#------------------------------------------------------------------------------------------------------------------------
+#' identifyGroupBoundaries: where avg and sd columns begin and end
+#'
+#' @rdname identifyGroupBoundaries
+#' @aliases identifyGroupBoundaries
+#'
+#' @export
+#'
+setMethod('identifyGroupBoundaries', 'ExcelProteomicsImporter',
+
+     function(obj){
+        colnames <- colnames(obj@state$rawTable)
+        avg.cols <- grep("avg", colnames)
+        sd.cols  <- grep("stdev", colnames)
+        if(obj@state$groupCount == 1){
+           obj@stage$group.1.avg <- c(avg.cols[1] + 1, sd.cols[1] - 1)
+           obj@stage$group.1.sd  <- c(sd.cols[1] + 1, length(colnames))
+           obj@stage$group.2.avg <- NA
+           obj@stage$group.2.sd <- NA
+           }
+        if(obj@state$groupCount == 2){
+           obj@stage$group.1.avg <- c(avg.cols[1] + 1, avg.cols[2] - 1)
+           obj@stage$group.1.sd  <- c(sd.cols[1] + 1,  sd.cols[2] - 1)
+           obj@stage$group.2.avg <- c(avg.cols[2] + 1, sd.cols[1] - 1)
+           obj@stage$group.2.sd <-  c(sd.cols[2] + 1,  length(colnames))
+           }
+        }) # identifyGroupBoundaries
+
+#------------------------------------------------------------------------------------------------------------------------
+#' get groupCount, the number of logically separate condition groups in the spreadsheet
+#'
+#' @rdname getGroupCount
+#' @aliases getGroupCount
+#'
+#' @export
+#'
+setMethod('getGroupCount', 'ExcelProteomicsImporter',
+
+     function(obj){
+        obj@stage$groupCount
+        })
+
+#------------------------------------------------------------------------------------------------------------------------
+#' get groupGroupBoundaries, the start and stop of columns for avg group/s, sd group/s
+#'
+#' @rdname getGroupBoundaries
+#' @aliases getGroupBoundaries
+#'
+#' @export
+#'
+setMethod('getGroupBoundaries', 'ExcelProteomicsImporter',
+
+     function(obj){
+         list(avg.1=obj@stage$group.1.avg,
+              avg.2=obj@stage$group.2.avg,
+              sd.1 =obj@stage$group.1.sd,
+              sd.2 =obj@stage$group.2.sd)
         })
 
 #------------------------------------------------------------------------------------------------------------------------
--- inst/unitTests/test_ExcelProteomicsImporter.R
+++ inst/unitTests/test_ExcelProteomicsImporter.R
@@ -3,9 +3,16 @@
 library(ExcelProteomicsImporter)
 library(RUnit)
 #------------------------------------------------------------------------------------------------------------------------
+test.files <- file.path(system.file(package="ExcelProteomicsImporter", "extdata"),
+                        c("test_data2_LCL57cell_line_experiment2.txt",
+                          "test_data3_PBMC_experiment.txt",
+                          "test_data4_Glioma_cell_line_experiment.txt",
+                          "test_data_LCL57cell_line_experiment.txt"))
+#------------------------------------------------------------------------------------------------------------------------
 runTests <- function()
 {
    test_ctor()
+   test_validateColumnNames()
 
 } # runTests
 #------------------------------------------------------------------------------------------------------------------------
@@ -13,15 +20,27 @@ test_ctor <- function()
 {
    message(sprintf("--- test_ctor"))
 
-   filename <- "test_data_LCL57cell_line_experiment.txt"
-   file.path <- system.file(package="ExcelProteomicsImporter", "extdata", filename)
-   checkTrue(file.exists(file.path))
-
-   importer <- ExcelProteomicsImporter(file.path)
-   tbl.raw <- getRawTable(importer)
-   checkEquals(dim(tbl.raw), c(37, 27))
+   for(file in test.files){
+      checkTrue(file.exists(file))
+      importer <- ExcelProteomicsImporter(file)
+      tbl.raw <- getRawTable(importer)
+      checkTrue(nrow(tbl.raw) %in% c(39, 37, 33, 37))
+      checkTrue(ncol(tbl.raw) %in% c(15, 37, 29, 27))
+      }
 
 } # test_ctor
+#------------------------------------------------------------------------------------------------------------------------
+test_validateColumnNames <- function()
+{
+   message(sprintf("--- test_validateColumnNames"))
+
+   for(file in test.files){
+      printf("--- %s", file)
+      importer <- ExcelProteomicsImporter(file)
+      checkTrue(validateColumnNames(importer))
+      }
+
+} # test_validateColumnNames
 #------------------------------------------------------------------------------------------------------------------------
 if(!interactive())
    runTests()
